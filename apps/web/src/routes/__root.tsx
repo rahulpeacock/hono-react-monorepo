e9ecfc76775f44e5e8b33ba513827934f7c8788b
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-12-25 16:42:44 +0530

    feat: add tanstack router devtools

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,24 +1,30 @@
-import { QueryProvider } from "@/web/client/providers/query-providers";
+import { QueryProvider } from '@/web/client/providers/query-providers';
+import { TanStackRouterDevtools } from '@/web/client/providers/router-devtools';
+
 // import { ThemeProvider } from '@/client/providers/theme-provider';
 // import { Toaster } from '@/components/ui/sonner';
-import type { QueryClient } from "@tanstack/react-query";
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { Fragment } from "react/jsx-runtime";
+import type { QueryClient } from '@tanstack/react-query';
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import React from 'react';
+import { Fragment } from 'react/jsx-runtime';
 
 interface MyRouterContext {
-	queryClient: QueryClient;
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-	component: RootRouteComponent,
+  component: RootRouteComponent,
 });
 
 function RootRouteComponent() {
-	return (
-		<Fragment>
-			<QueryProvider>
-				<Outlet />
-			</QueryProvider>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <QueryProvider>
+        <Outlet />
+      </QueryProvider>
+      <React.Suspense>
+        <TanStackRouterDevtools />
+      </React.Suspense>
+    </Fragment>
+  );
 }
