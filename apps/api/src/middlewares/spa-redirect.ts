69b80315ac017530d3ff83d76ac7c02168e8ebd5
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-01-02 17:11:20 +0530

    enhance: spa-redirects

--- apps/api/src/middlewares/spa-redirect.ts
+++ apps/api/src/middlewares/spa-redirect.ts
@@ -3,17 +3,18 @@ import { serveStatic } from '@hono/node-server/serve-static';
 import { createMiddleware } from 'hono/factory';
 import { BASE_PATH } from '../lib/constants';
 
-export const spaFavicon = createMiddleware(serveStatic({ path: `${env.SPA_ROOT_DIR}/vite.svg` }));
+export const spaFavicon = createMiddleware(serveStatic({ root: env.SPA_ROOT_DIR, path: 'vite.svg' }));
 
 export const spaRedirect = createMiddleware(async (c, next) => {
   if (c.req.path.startsWith(BASE_PATH)) {
     return next();
   }
 
+  // SPA redirect for static assets (css, js)
   if (c.req.path.startsWith(env.SPA_ASSETS_PATH)) {
     return serveStatic({ root: env.SPA_ROOT_DIR })(c, next);
   }
 
   // SPA redirect to /index.html
-  return serveStatic({ path: `${env.SPA_ROOT_DIR}/index.html` })(c, next);
+  return serveStatic({ root: env.SPA_ROOT_DIR, path: 'index.html' })(c, next);
 });
